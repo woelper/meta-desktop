0a1eecdf4afcf8abbcf37ac8da7e3f476cb1c1ef
Johann Woelper <woelper@gmail.com>
2016-01-14 20:28:37 +0100

    xfce theme and sourceswq

--- media/themes/source/xfce/release.sh
+++ media/themes/source/xfce/release.sh
@@ -3,12 +3,22 @@ rm *.png
 
 python compile_xfce.py
 
+for i in top menu left title bottom right
+do
+    rm $i*toggle*.png
+done
 #for i in *.png
 #do
 #    convert $i -trim +repage $i
 #done
 
-for i in *.png
-do
-    convert $i ../../Meta/xfwm4/$(basename ${i%.*}.xpm)
-done
+function cnv {
+
+    for i in *.png
+    do
+        convert $i ../../Meta${1}/xfwm4/$(basename ${i%.*}.xpm)
+    done
+}
+
+
+cnv $1
